56ad7231aefd5ce740881d196c068c97dcfe655d
ergashev <erjan.ergashev@mail.ru>
2021-12-02 20:08:40 +0500

    first commit

---
+++ guangzhou/js/main.js
@@ -0,0 +1,15 @@
+
+try {
+    $(document).ready(function() {
+        $(".fancybox").fancybox();
+    });
+} catch (e) {
+
+}
+
+try {
+    $('.phone-musk').inputmask("+\\9\\9\\ (89)9 999-99-99");
+    $('.date-musk').inputmask("99/99/9999");
+} catch (e) {
+
+}
